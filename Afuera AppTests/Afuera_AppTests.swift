1a02894531e4ff4a902aa7898816d25486833d9e
Jake Lowry <s027614@students.lmsd.org>
2021-03-11 12:00:45 -0500

    initial commit

---
+++ Afuera AppTests/Afuera_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Afuera_AppTests.swift
+//  Afuera AppTests
+//
+//  Created by Jacob Lowry (student LM) on 3/11/21.
+//  Copyright © 2021 Jacob Lowry (student LM). All rights reserved.
+//
+
+import XCTest
+@testable import Afuera_App
+
+class Afuera_AppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
